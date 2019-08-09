b70b88878453b9b5a01e505ca9758d5357db38ba
Rich Frost <richard@ipponsolutions.co.uk>
2019-08-09 17:34:21 +0100

    Added scripting to make master seal list for mappings

---
+++ mappings.js
@@ -0,0 +1,40 @@
+const mappings = require('./seal-mappings/seals.json');
+
+const processMappings = () => {
+    let processedSeals = [];
+    let masterList = [];
+    const uniq = mappings.reduce((seals, item) => {
+        const { id, dup } = item;
+        if (dup !== '' && !processedSeals.includes(id)) {
+            // Add key seal to processed list
+            processedSeals.push(id);
+            let master = id;
+            const dupSeals = dup.split(' ');
+            const index = dupSeals.indexOf(id);
+            if (index > -1) {
+                dupSeals.splice(index, 1);
+            }
+
+            // Add the duplicated seals to processed list too so we don't re-add them later
+            processedSeals.push(...dupSeals);
+
+            // Check if master already set as it'll appear in the processed list
+            dupSeals.map(s => {
+                if (masterList.includes(s)) {
+                    master = s;
+                }
+            });
+
+            dupSeals.map(s => (seals[s] = master));
+            seals[id] = master;
+            masterList.push(master);
+        }
+        return seals;
+    }, {});
+    //S1013
+    fs.writeFileSync(`seal-mappings/mappings.json`, JSON.stringify(uniq, null, 2));
+};
+
+module.exports = {
+    processMappings
+};
