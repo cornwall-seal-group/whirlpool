7fbfa87c418a57e5e5bed62c6db949f21f482276
Rich Frost <richard@ipponsolutions.co.uk>
2019-08-13 17:17:02 +0100

    Remove any duplicate new-ids as well

--- src/utils/process-images.js
+++ src/utils/process-images.js
@@ -138,6 +138,7 @@ const extractSealsFromSlides = () => {
     });
 
     removeDuplicateImagesFromFolders();
+    removeDuplicateNoIdImages();
     reSyncMinio();
 
     return { processed: slideSeals };
@@ -182,6 +183,42 @@ const removeDuplicateImagesFromFolders = () => {
 
             fs.renameSync(existingFilePath, newFilePath);
         });
+    });
+};
+
+const removeDuplicateNoIdImages = () => {
+    const folder = imageOutputDir + 'no-ids';
+    findDuplicateFiles(
+        folder,
+        {
+            silent: true,
+            md5SkipSaving: true,
+            md5SkipLoading: true
+        },
+        function(err, groups) {
+            if (err) return console.error(err);
+            groups.forEach(function(group) {
+                // loop starts at index 1
+                // first item will be untouched
+                for (var i = 1; i < group.length; i++) {
+                    fs.unlinkSync(group[i].path);
+                }
+            });
+        }
+    );
+
+    //Now re-number the images in the folder so they are 1-*
+    const files = fs.readdirSync(folder);
+
+    files.forEach((file, index) => {
+        const filePath = file.split('.');
+        const ext = filePath[filePath.length - 1];
+        const formattedNum = index + 1;
+
+        const existingFilePath = path.join(folder, file);
+        const newFilePath = path.join(folder, 'new-' + formattedNum.toString() + '.' + ext);
+
+        fs.renameSync(existingFilePath, newFilePath);
     });
 };
 
