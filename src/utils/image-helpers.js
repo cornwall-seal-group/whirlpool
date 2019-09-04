f96afb4d43dc496c98b94262bbe2f3e58c50face
Rich Frost <richard@ipponsolutions.co.uk>
2019-09-04 14:01:05 +0100

    Handle no-ids folder in removing unsupported image types, and re-naming

--- src/utils/image-helpers.js
+++ src/utils/image-helpers.js
@@ -9,36 +9,41 @@ const imageOutputDir = config.sealImagesOutputDir;
 const unsupportedImageTypes = ['tiff', 'tif'];
 const unsupportedFiles = ['emf', 'wdp'];
 
+const removeUnsupportedTypesForSeal = ({ seal, hasOriginals = true }) => {
+    const sealFolder = getSealFolder(seal);
+    const folder = imageOutputDir + sealFolder + hasOriginals ? '/originals' : '';
+    console.log('Checking for unsupported images types in', folder);
+
+    const files = fs.readdirSync(folder);
+
+    files.forEach(file => {
+        const filePath = file.split('.');
+        const ext = filePath[filePath.length - 1];
+        const existingFilePath = path.join(folder, file);
+
+        // Remove file if it is unsupported
+        if (unsupportedFiles.includes(ext)) {
+            console.log(file, `unsupported type ${ext}, removing`);
+            fs.unlinkSync(existingFilePath);
+        }
+
+        // Rename file if it is unsupported type
+        if (unsupportedImageTypes.includes(ext)) {
+            console.log(file, 'unsupported file type, renaming to jpg');
+            const newFilePath = folder + filePath + '.jpg';
+            console.log('About to run', `convert ${existingFilePath} ${newFilePath}`);
+            execSync(`convert ${existingFilePath} ${newFilePath}`);
+            // Remove old file
+            fs.unlinkSync(existingFilePath);
+        }
+    });
+};
 const handleUnsupportedImageTypes = foundSeals => {
     foundSeals.forEach(seal => {
-        const sealFolder = getSealFolder(seal);
-        const folder = imageOutputDir + sealFolder + '/originals';
-        console.log('Checking for unsupported images types in', folder);
-
-        const files = fs.readdirSync(folder);
-
-        files.forEach(file => {
-            const filePath = file.split('.');
-            const ext = filePath[filePath.length - 1];
-            const existingFilePath = path.join(folder, file);
-
-            // Remove file if it is unsupported
-            if (unsupportedFiles.includes(ext)) {
-                console.log(file, `unsupported type ${ext}, removing`);
-                fs.unlinkSync(existingFilePath);
-            }
-
-            // Rename file if it is unsupported type
-            if (unsupportedImageTypes.includes(ext)) {
-                console.log(file, 'unsupported file type, renaming to jpg');
-                const newFilePath = folder + filePath + '.jpg';
-                console.log('About to run', `convert ${existingFilePath} ${newFilePath}`);
-                execSync(`convert ${existingFilePath} ${newFilePath}`);
-                // Remove old file
-                fs.unlinkSync(existingFilePath);
-            }
-        });
+        removeUnsupportedTypesForSeal({ seal });
     });
+    // Remove from no-ids folder too
+    removeUnsupportedTypesForSeal({ seal: 'no-ids', hasOriginals: false });
 };
 
 const removeDuplicateImagesFromFolders = foundSeals => {
